161edb23ffc882663f2d61ce3e76c518a1c8e5c3
Kristof-Kolak <krzysztof.kolak.02@gmail.com>
2026-04-17 20:39:55 +0200

    Fixed logic

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ enum Side{
 }
 
 
-const WIDTH: i32 = 200;
-const HEIGHT: i32 = 200;
+const WIDTH: i32 = 1000;
+const HEIGHT: i32 = 1000;
 
 
 
@@ -21,7 +21,7 @@ async fn main() {
 
     set_window_size(WIDTH as u32, HEIGHT as u32);
     
-    
+    let mut item_counter = 0;
 
     loop {
 
@@ -33,6 +33,7 @@ async fn main() {
             if (x < WIDTH as f32 && x >= 0.0) && (y < HEIGHT as f32 && y >= 0.0){
                 if screen_map[y as usize][x as usize] != 1{
                     screen_map[y as usize][x as usize] = 1;
+                    item_counter +=1;
                 }
             }
         }
@@ -40,6 +41,7 @@ async fn main() {
             if (x < WIDTH as f32 && x >= 0.0) && (y < HEIGHT as f32 && y >= 0.0){
                 if screen_map[y as usize][x as usize] == 1{
                     screen_map[y as usize][x as usize] = 0;
+                    item_counter -=1;
                 }
             }
         }
@@ -52,7 +54,7 @@ async fn main() {
                     //bottom line condition
                 }else if screen_map[index_y as usize][index_x as usize] == 1 {
                     if index_y+1 < HEIGHT{                    
-                        if screen_map[(index_y+1) as usize][index_x as usize] == 0{
+                        if screen_map[(index_y+1) as usize][index_x as usize] == 0 && new_map[(index_y+1) as usize][index_x as usize] == 0{ 
                             //bottom simple movement
                             new_map[index_y as usize][index_x as usize] = 0;
                             new_map[(index_y+1) as usize][index_x as usize] = 1;
@@ -62,19 +64,19 @@ async fn main() {
                                 let first_side = [Side::Left, Side::Right].choose().copied();
                                 match first_side {
                                     Some(Side::Left) =>{
-                                        if screen_map[(index_y+1) as usize][(index_x-1) as usize] == 0{
+                                        if screen_map[(index_y+1) as usize][(index_x-1) as usize] == 0 && new_map[(index_y+1) as usize][(index_x-1) as usize] == 0{
                                             new_map[index_y as usize][index_x as usize] = 0;
                                             new_map[(index_y+1) as usize][(index_x-1) as usize] = 1;
-                                        }else if screen_map[(index_y+1) as usize][(index_x+1) as usize] == 0{
+                                        }else if screen_map[(index_y+1) as usize][(index_x+1) as usize] == 0 && new_map[(index_y+1) as usize][(index_x+1) as usize] == 0{
                                             new_map[index_y as usize][index_x as usize] = 0;
                                             new_map[(index_y+1) as usize][(index_x+1) as usize] = 1;
                                         }
                                     }
                                     Some(Side::Right) =>{
-                                        if screen_map[(index_y+1) as usize][(index_x+1) as usize] == 0{
+                                        if screen_map[(index_y+1) as usize][(index_x+1) as usize] == 0 && new_map[(index_y+1) as usize][(index_x+1) as usize] == 0{
                                             new_map[index_y as usize][index_x as usize] = 0;
                                             new_map[(index_y+1) as usize][(index_x+1) as usize] = 1;
-                                        }else if screen_map[(index_y+1) as usize][(index_x-1) as usize] == 0{
+                                        }else if screen_map[(index_y+1) as usize][(index_x-1) as usize] == 0 && new_map[(index_y+1) as usize][(index_x-1) as usize] == 0{
                                             new_map[index_y as usize][index_x as usize] = 0;
                                             new_map[(index_y+1) as usize][(index_x-1) as usize] = 1;
                                         } 
@@ -85,12 +87,12 @@ async fn main() {
                                 }
 
                             }else if index_x - 1 >= 0{ 
-                                if screen_map[(index_y+1) as usize][(index_x-1) as usize] == 0{
+                                if screen_map[(index_y+1) as usize][(index_x-1) as usize] == 0 && new_map[(index_y+1) as usize][(index_x-1) as usize] == 0{
                                     new_map[index_y as usize][index_x as usize] = 0;
                                     new_map[(index_y+1) as usize][(index_x-1) as usize] = 1;
                                 }
                             }else if index_x + 1 < WIDTH{
-                                if screen_map[(index_y+1) as usize][(index_x+1) as usize] == 0{
+                                if screen_map[(index_y+1) as usize][(index_x+1) as usize] == 0 && new_map[(index_y+1) as usize][(index_x+1) as usize] == 0{
                                     new_map[index_y as usize][index_x as usize] = 0;
                                     new_map[(index_y+1) as usize][(index_x+1) as usize] = 1;
                                 }
@@ -101,16 +103,19 @@ async fn main() {
             }
         } 
         screen_map = new_map;
-        
+        let mut new_map_counter = 0;
         for (index_y, row) in screen_map.iter().enumerate(){
             for (index_x, _) in row.iter().enumerate(){
                 if screen_map[index_y][index_x] == 1{
+                    new_map_counter += 1;
                     draw_rectangle(index_x as f32, index_y as f32, 1.0, 1.0, YELLOW);
 
                 }
             }
         }
-
+        if new_map_counter != item_counter{
+            println!("new_map_counter: {} is not equal item_counter: {}", new_map_counter, item_counter)
+        }
         next_frame().await
     }
 }
